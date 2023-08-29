b8d9ebe1b22e80359fdfebe11b6de71881bedb8f
igtoj <j.iglandinitorres@gmail.com>
2023-08-28 22:05:34 -0500

    .

--- R/ExerG.R
+++ R/ExerG.R
@@ -1,3 +1,23 @@
+#' R DATA ANALYSIS
+#'
+#'  In this work, an analysis and different exercises are carried out with data from the nycflights13 package, using additional libraries such as Tidyverse, knitr, among other additional functions
+#'
+#' @param x The exercise number (ranging from 1 to 6).
+#' @return The solution for the specified exercise.
+#' @export
+#'
+#' @import tidyverse
+#' @import nycflights13: This library provides flight data to analyze and solve the proposed exercises
+#' @import knitr
+#'
+#' @examples
+#' # Retrieve the solution for exercise 1
+#' ExerG1
+#'
+#' # Retrieve the solution for exercise 6
+#' ExerG6
+# Function to select questions and answers based on the exercise number
+
 ExerG<- function(x){
   if (x==1){
     return(ExerG1())
